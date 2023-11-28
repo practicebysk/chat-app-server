49f552429d8a67a086948ba62f78059aaef5fd62
kinar <nothing225200@gmail.com>
2023-11-27 21:24:57 -0500

    IC-2

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const app = express();
 require("dotenv").config();
 
 app.use(cors());
-app.use(express.json());
+app.use(express.json()); 
 
 app.use("/api/auth",userRoutes)
 const connectToMongo = async () => {
